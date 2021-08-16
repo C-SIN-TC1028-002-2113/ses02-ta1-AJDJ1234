5dac3df65319956953ad8e63a33c42ad9632f888
AJDJ1234 <a01747819@itesm.mx>
2021-08-16 20:36:20 +0000

    Tareas resueltas

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = input("Dame tu edad: ")
+    año = input("Dame el año actual: ")
+    añofinal = (100-int(edad)) + int(año) 
 
-
-
+    print("Cumplirás 100 años en el año:",añofinal)
 
 if __name__ == '__main__':
     main()
--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    naci = input("Dame el año de nacimiento: ")
+    año = input("Dame el año actual: ")
+    lustros = (int(año) - int(naci))/ 5
 
-
+    print("Los lustros que has vivido son:",lustros)
 
 if __name__ == '__main__':
     main()
